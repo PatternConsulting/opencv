0a4c6f508dbf17f18cc2d88f3cb81baef4dc4367
Michael Ahlers <michael@ahlers.co>
2014-05-19 17:03:09 -0400

    More appropriate name.

--- src/test/java/nu/pattern/LoadLibraryRunListener.java
+++ src/test/java/nu/pattern/LoadLibraryRunListener.java
@@ -3,7 +3,7 @@ package nu.pattern;
 import org.junit.runner.Description;
 import org.junit.runner.notification.RunListener;
 
-public class OpenCVRunListener extends RunListener {
+public class LoadLibraryRunListener extends RunListener {
   @Override
   public void testRunStarted(final Description description) throws Exception {
     super.testRunStarted(description);
